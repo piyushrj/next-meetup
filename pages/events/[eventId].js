dcc64390b60d17b80c7d941d20dd7bcfe82b695a
Piyush Raj <piyushrajiitr@gmail.com>
2022-10-04 15:51:52 +0530

    added event filtering functionality and updated styles

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,11 @@
+import { Fragment } from 'react';
 import { useRouter } from 'next/router'
 import { getEventById } from '../../dummy-data';
+
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/ui/error-alert';
 
 function EventDetailPage() {
     const router = useRouter();
@@ -7,13 +13,22 @@ function EventDetailPage() {
     const event = getEventById(eventId);
 
     if (!event) {
-        return <p>No event found!</p>
+        return <ErrorAlert><p>No event found!</p></ErrorAlert>
     }
 
     return (
-        <div>
-            <h1>Individual Events Page - Event {router.query.eventId}</h1>
-        </div>
+        <Fragment>
+            <EventSummary title={event.title} />
+            <EventLogistics
+                date={event.date}
+                address={event.location}
+                image={event.image}
+                imageAlt={event.title}
+            />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
     )
 }
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,23 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+
 function EventsPage() {
+    const router = useRouter();
+    const events = getAllEvents();
+
+    function findEventsHandler(year, momth) {
+        const fullPath = `/events/${year}/${momth}`;
+        router.push(fullPath);
+    }
+
     return (
-        <div>
-            <h1>All Events</h1>
-        </div>
+        <Fragment>
+            <EventsSearch onSearch={findEventsHandler} />
+            <EventList items={events} />
+        </Fragment>
     )
 }
 
