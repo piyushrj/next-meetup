8c351fa0cdda64964477c488a7a74eeb7fdd865d
Piyush Raj <piyushrajiitr@gmail.com>
2022-10-11 10:23:38 +0530

    added data fetching from cloud database with ssg on homepage and events page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,16 +1,14 @@
 import { Fragment } from 'react';
-import { useRouter } from 'next/router'
-import { getEventById } from '../../dummy-data';
+
+import { getEventById, getAllEvents } from '../../helpers/api-utils';
 
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
 
-function EventDetailPage() {
-    const router = useRouter();
-    const eventId = router.query.eventId;
-    const event = getEventById(eventId);
+function EventDetailPage(props) {
+    const event = props.event;
 
     if (!event) {
         return <ErrorAlert><p>No event found!</p></ErrorAlert>
@@ -30,6 +28,25 @@ function EventDetailPage() {
             </EventContent>
         </Fragment>
     )
+}
+
+export async function getStaticProps(context) {
+    const eventId = context.params.eventId;
+    const event = await getEventById(eventId);
+    return {
+        props: {
+            event
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    const events = await getAllEvents();
+    const paths = events.map(event => ({ params: { eventId: event.id } }));
+    return {
+        paths: paths,
+        fallback: false
+    }
 }
 
 export default EventDetailPage;
