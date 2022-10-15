a93d3e4cb4b17ae3690b113dbba5bb96daa6ed39
Piyush Raj <piyushrajiitr@gmail.com>
2022-10-15 20:01:17 +0530

    added api routes and data handling with cloud mongodb

---
+++ helpers/db-utils.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+
+export async function connectDatabase() {
+    const client = await MongoClient.connect(
+        "mongodb+srv://piyush:YDIXGWDtIqdgL7gd@cluster0.vytrj6d.mongodb.net/events?retryWrites=true&w=majority"
+    )
+    return client;
+}
+
+export async function insertDocument(client, collection, document) {
+    const db = client.db();
+    const result = await db.collection(collection).insertOne(document);
+    return result;
+}
+
+export async function getAllDocuments(client, collection, sort) {
+    const db = client.db();
+    const documents = await db.collection(collection).find().sort(sort).toArray();
+    return documents;
+}
---
+++ pages/api/newsletter.js
@@ -0,0 +1,34 @@
+import { connectDatabase, insertDocument } from '../../helpers/db-utils';
+
+async function handler(req, res) {
+    if (req.method === 'POST') {
+        const userEmail = req.body.email;
+
+        // server side validation for email
+        if (!userEmail || !userEmail.includes('@')) {
+            res.status(422).json({ message: "Invalid Email Address." });
+            return
+        }
+
+        let client;
+
+        try {
+            client = await connectDatabase();
+        } catch (error) {
+            res.status(500).json({ message: "Connecting to the database failed!" });
+            return
+        }
+
+        try {
+            await insertDocument(client, "newsletter", { email: userEmail });
+            client.close();
+        } catch (error) {
+            res.status(500).json({ message: "Inserting data failed!" });
+            return
+        }
+
+        res.status(201).json({ message: "Signed up successfully!" })
+    }
+}
+
+export default handler;
