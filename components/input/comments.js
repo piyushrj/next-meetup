be382c7cdfbfda84345b9fbce24d4cfcbf66ee43
Piyush Raj <piyushrajiitr@gmail.com>
2022-10-16 16:02:10 +0530

    added notification functionality

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,21 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
+import NotificationContext from '../../store/notification-context';
 
 function Comments(props) {
   const { eventId } = props;
 
+  const notificationCtx = useContext(NotificationContext);
+
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
 
+  const [isFetchingComments, setIsFetchingComments] = useState(false);
+
   useEffect(() => {
     if (showComments) {
+      setIsFetchingComments(true);
       fetch('/api/comments/' + eventId)
         .then((response) => response.json())
         .then((data) => {
           setComments(data.comments);
+          setIsFetchingComments(false);
         })
     }
   }, [showComments])
@@ -26,6 +33,12 @@ function Comments(props) {
 
   function addCommentHandler(commentData) {
     // send data to API
+    notificationCtx.showNotification({
+      title: 'Adding comment ...',
+      message: 'Adding new comment.',
+      status: 'pending'
+    })
+
     fetch('/api/comments/' + eventId, {
       method: 'POST',
       body: JSON.stringify(commentData),
@@ -33,8 +46,29 @@ function Comments(props) {
         'Content-Type': 'application/json'
       }
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        return response.json().then((data) => {
+          throw new Error(data.message || "Something went wrong.")
+        });
+      })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: 'Success!',
+          message: 'Successfully added comment.',
+          status: 'success'
+        })
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: 'Error!',
+          message: error.message || "Something went wrong!",
+          status: 'error'
+        })
+      });
   }
 
   return (
@@ -43,7 +77,9 @@ function Comments(props) {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments} />}
+      {showComments && !isFetchingComments && <CommentList items={comments} />}
+      {showComments && isFetchingComments && <p>Loading...</p>}
+
     </section>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
 import Head from 'next/head';
 import '../styles/globals.css';
 import Layout from '../components/layout/layout';
+import Notification from '../components/ui/notification';
+import { NotificationContextProvider } from '../store/notification-context';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name='description' content='NextJS Events' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name='description' content='NextJS Events' />
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   )
 }
 
